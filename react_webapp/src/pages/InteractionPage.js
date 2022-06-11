aad8dbbfab1d31f3ee19713f84326482d0f008cc
ChrisFu2010 <fuyue2010cal@gmail.com>
2022-06-11 11:15:24 -0700

    add reminder press button sentence to MIC

--- react_webapp/src/pages/InteractionPage.js
+++ react_webapp/src/pages/InteractionPage.js
@@ -5,9 +5,10 @@ export default function InteractionPage(props) {
   return (
     <main>
       <div className="profiles">
-        <Avatar isSpeaking={props.botSpeaking} src="/img/s_profile.png"/>
-        <Avatar isSpeaking={!props.botSpeaking} src="/img/k_profile.png"/>
+        <Avatar isSpeaking={props.botSpeaking} src="/img/s_profile.png" />
+        <Avatar isSpeaking={!props.botSpeaking} src="/img/k_profile.png" />
       </div>
+
     </main>
   );
 }
